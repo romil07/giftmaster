12182ad4ccbb825c8ba4d5a7a0a8c740c4a64910
rohan.ghosh <rohan.ghosh@flipkart.com>
2016-07-21 19:40:33 +0530

    Todo classes

---
+++ manager/src/main/java/fk/sp/st/manager/action/GetRecommendedProductForEmailId.java
@@ -0,0 +1,7 @@
+package fk.sp.st.manager.action;
+
+/**
+ * Created by rohan.ghosh on 21/07/16.
+ */
+public class GetRecommendedProductForEmailId {
+}
---
+++ manager/src/main/java/fk/sp/st/manager/clients/SherlockClient.java
@@ -0,0 +1,7 @@
+package fk.sp.st.manager.clients;
+
+/**
+ * Created by rohan.ghosh on 21/07/16.
+ */
+public class SherlockClient {
+}
