f6aafcb79d2affb2b89d0728ddb7b70dbb244ff9
Romil Goyal <romil.goyal@flipkart.com>
2016-07-22 07:55:42 +0530

    Get wishlist from user id

--- manager/src/main/java/fk/sp/st/manager/GiftMasterResource.java
+++ manager/src/main/java/fk/sp/st/manager/GiftMasterResource.java
@@ -3,32 +3,21 @@ package fk.sp.st.manager;
 
 import com.codahale.metrics.annotation.ExceptionMetered;
 import com.codahale.metrics.annotation.Timed;
-
-import org.springframework.jdbc.core.JdbcTemplate;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-
-import javax.inject.Inject;
-import javax.inject.Provider;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-
 import fk.sp.st.manager.action.GetEventDetails;
 import fk.sp.st.manager.action.GetRecommendedProductForEmailId;
 import fk.sp.st.manager.clients.PriceFromProdIdClient;
 import fk.sp.st.manager.clients.UserServiceClient;
 import fk.sp.st.manager.model.ListingInfo;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.inject.Inject;
+import javax.inject.Provider;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.*;
 
 @Slf4j
 @Produces(MediaType.APPLICATION_JSON)
@@ -36,102 +25,117 @@ import lombok.extern.slf4j.Slf4j;
 @Path("/gift-master")
 public class GiftMasterResource {
 
-  private PriceFromProdIdClient priceFromProdIdClient;
-  private final GetRecommendedProductForEmailId getRecommendedProductForEmailId;
-  private JdbcTemplate jdbcTemplate;
-  private final String
-      query =
-      "select fsn from fsn_details where age = ? and sex = ? and price <= ? and occasion = ?";
-  private GetEventDetails getEventDetails;
-  private Provider<UserServiceClient> userServiceClient;
+    private PriceFromProdIdClient priceFromProdIdClient;
+    private final GetRecommendedProductForEmailId getRecommendedProductForEmailId;
+    private JdbcTemplate jdbcTemplate;
+    private final String
+            query =
+            "select fsn from fsn_details where age = ? and sex = ? and price <= ? and occasion = ?";
+    private GetEventDetails getEventDetails;
+    private Provider<UserServiceClient> userServiceClient;
 
 
-  @Inject
-  public GiftMasterResource(PriceFromProdIdClient priceFromProdIdClient,
-                            GetRecommendedProductForEmailId getRecommendedProductForEmailId,
-                            JdbcTemplate jdbcTemplate, GetEventDetails getEventDetails,
-                            Provider<UserServiceClient> userServiceClient) {
-    this.priceFromProdIdClient = priceFromProdIdClient;
-    this.getRecommendedProductForEmailId = getRecommendedProductForEmailId;
-    this.jdbcTemplate = jdbcTemplate;
-    this.getEventDetails = getEventDetails;
-    this.userServiceClient = userServiceClient;
-  }
-
-
-  @GET
-  @Timed
-  @Path("/getBestPrice")
-  public ListingInfo execute(@QueryParam("product_id") String productId) {
-    this.getRecommendedFSNs(25, "Male", 1000, "Birthday");
-    priceFromProdIdClient.setProductId(productId);
-    Object response = priceFromProdIdClient.run();
-    Integer price = new Integer(Integer.MAX_VALUE);
-    String listing_id = "";
-    LinkedHashMap<String, Object>
-        map =
-        (LinkedHashMap<String, Object>) ((LinkedHashMap) ((LinkedHashMap) ((LinkedHashMap) response)
-            .get("response")).get("RESPONSES")).get("Pricing Response");
-    for (Map.Entry entry : map.entrySet()) {
-      if (price > (Integer) ((LinkedHashMap) entry.getValue()).get("fsp")) {
-        price = (Integer) ((LinkedHashMap) entry.getValue()).get("fsp");
-        listing_id = entry.getKey().toString();
-      }
+    @Inject
+    public GiftMasterResource(PriceFromProdIdClient priceFromProdIdClient,
+                              GetRecommendedProductForEmailId getRecommendedProductForEmailId,
+                              JdbcTemplate jdbcTemplate, GetEventDetails getEventDetails,
+                              Provider<UserServiceClient> userServiceClient) {
+        this.priceFromProdIdClient = priceFromProdIdClient;
+        this.getRecommendedProductForEmailId = getRecommendedProductForEmailId;
+        this.jdbcTemplate = jdbcTemplate;
+        this.getEventDetails = getEventDetails;
+        this.userServiceClient = userServiceClient;
     }
 
-    ListingInfo listingInfo = new ListingInfo();
-    listingInfo.setListingId(listing_id);
-    listingInfo.setPrice(price);
-    return listingInfo;
-  }
 
-  @GET
-  @Timed
-  @ExceptionMetered
-  @Path("/getRecommendedProducts/{accID}")
-  public Object getRecommended(@PathParam("accID") String accId) throws Exception {
+    @GET
+    @Timed
+    @Path("/getBestPrice")
+    public ListingInfo execute(@QueryParam("product_id") String productId) {
+        this.getRecommendedFSNs(25, "Male", 1000, "Birthday");
+        priceFromProdIdClient.setProductId(productId);
+        Object response = priceFromProdIdClient.run();
+        Integer price = new Integer(Integer.MAX_VALUE);
+        String listing_id = "";
+        LinkedHashMap<String, Object>
+                map =
+                (LinkedHashMap<String, Object>) ((LinkedHashMap) ((LinkedHashMap) ((LinkedHashMap) response)
+                        .get("response")).get("RESPONSES")).get("Pricing Response");
+        for (Map.Entry entry : map.entrySet()) {
+            if (price > (Integer) ((LinkedHashMap) entry.getValue()).get("fsp")) {
+                price = (Integer) ((LinkedHashMap) entry.getValue()).get("fsp");
+                listing_id = entry.getKey().toString();
+            }
+        }
 
-    log.info("Recommended Products for {}", accId);
-    return getRecommendedProductForEmailId.invoke(accId);
-  }
+        ListingInfo listingInfo = new ListingInfo();
+        listingInfo.setListingId(listing_id);
+        listingInfo.setPrice(price);
+        return listingInfo;
+    }
 
+    @GET
+    @Timed
+    @ExceptionMetered
+    @Path("/getRecommendedProducts/{accID}")
+    public Object getRecommended(@PathParam("accID") String accId) throws Exception {
 
-  @GET
-  @Path("/event/details/{user_id}")
-  @Timed
-  @ExceptionMetered
-  public GetEventDetails.EventDetails getEventDetails(@PathParam("user_id") String userId) {
-
-    log.info(
-        "getEventDetails for {}", userId);
-
-    return getEventDetails.getEventDetails(userId);
-  }
-
-  @GET
-  @Path("/user/details")
-  @Timed
-  @ExceptionMetered
-  public UserServiceClient.UserInfo getAccountIdFromEmail(@QueryParam("email") String email) {
-
-    log.info(
-        "get User Details for {}", email);
-    return userServiceClient.get().withEmailId(email).run();
+        log.info("Recommended Products for {}", accId);
+        return getRecommendedProductForEmailId.invoke(accId);
+    }
 
 
-  }
+    @GET
+    @Path("/event/details/{user_id}")
+    @Timed
+    @ExceptionMetered
+    public GetEventDetails.EventDetails getEventDetails(@PathParam("user_id") String userId) {
 
-  private List<String> getRecommendedFSNs(int age, String sex, int budget, String occasion) {
-    List<String> rows = jdbcTemplate
-        .query(String.format(query, "fsn_details"), new Object[]{age, sex, budget, occasion},
-               this::readRow);
-    return rows;
-  }
+        log.info(
+                "getEventDetails for {}", userId);
 
-  private String readRow(ResultSet rs, int n) throws SQLException {
-    String row;
-    row = rs.getString("fsn");
-    return row;
-  }
+        return getEventDetails.getEventDetails(userId);
+    }
 
+    @GET
+    @Path("/user/details")
+    @Timed
+    @ExceptionMetered
+    public UserServiceClient.UserInfo getAccountIdFromEmail(@QueryParam("email") String email) {
+
+        log.info(
+                "get User Details for {}", email);
+        return userServiceClient.get().withEmailId(email).run();
+
+
+    }
+
+    private List<String> getRecommendedFSNs(int age, String sex, int budget, String occasion) {
+        List<String> rows = jdbcTemplate
+                .query(String.format(query, "fsn_details"), new Object[]{age, sex, budget, occasion},
+                        this::readRow);
+        return rows;
+    }
+
+    private String readRow(ResultSet rs, int n) throws SQLException {
+        String row;
+        row = rs.getString("fsn");
+        return row;
+    }
+
+    @GET
+    @Timed
+    @Path("/wishlist")
+    public List<String> getWishList(@QueryParam("user_id") String userId) {
+        Map<String, List<String>> map = new HashMap<>();
+        List<String> list = new ArrayList<>();
+        list.add("afjadgh");
+        list.add("afferwgh");
+        list.add("afjafsdgs");
+        list.add("agwerge");
+        list.add("agwegeg");
+        map.put("jafsa", list);
+
+        return map.get(userId);
+    }
 }
